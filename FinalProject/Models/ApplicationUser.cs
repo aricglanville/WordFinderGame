869bdeb26e0bfe0a1bc38cbea543fde7f0082e59
Kolby Croshaw <kolbycroshaw@gmail.com>
2022-07-20 18:21:56 -0600

    Adding migration files

---
+++ FinalProject/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace FinalProject.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string UserName { get; set; }
+    }
+}
+
