9807f6a10b2fedcc91bb7aa874a092cb687c1184
Kolby Croshaw <kolbycroshaw@gmail.com>
2022-07-21 16:25:02 -0600

    Got drawing on the canvas working

--- FinalProject/wwwroot/js/site.js
+++ FinalProject/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function drawGameBoard() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.rect(0, 0, 100, 100);
+    ctx.rect(0, 100, 100, 100);
+    ctx.rect(0, 200, 100, 100);
+    ctx.rect(0, 300, 100, 100);
+    ctx.rect(100, 0, 100, 100);
+    ctx.rect(100, 100, 100, 100);
+    ctx.rect(100, 200, 100, 100);
+    ctx.rect(100, 300, 100, 100);
+    ctx.rect(200, 0, 100, 100);
+    ctx.rect(200, 100, 100, 100);
+    ctx.rect(200, 200, 100, 100);
+    ctx.rect(200, 300, 100, 100);
+    ctx.rect(300, 0, 100, 100);
+    ctx.rect(300, 100, 100, 100);
+    ctx.rect(300, 200, 100, 100);
+    ctx.rect(300, 300, 100, 100);
+    ctx.stroke();
+}
