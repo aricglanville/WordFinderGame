429f8910c28c5741ad7972c01f20f14352675ba9
Kolby Croshaw <kolbycroshaw@gmail.com>
2022-08-01 15:55:57 -0600

    Signalr showing the grid once two users join.

--- FinalProject/Hubs/ChatHub.cs
+++ FinalProject/Hubs/ChatHub.cs
@@ -4,9 +4,29 @@ namespace FinalProject.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public static int count = 0;
+
+        public async Task JoinGame(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            count++;
+            var userName = Context.User.Identity.Name;
+            await base.OnConnectedAsync();
+
+            if (count == 2)
+            {
+                
+                await Clients.All.SendAsync("ShowGrid");
+                
+            }
+            else
+            {
+                await Clients.All.SendAsync("HideGrid");
+            }
         }
     }
 }
