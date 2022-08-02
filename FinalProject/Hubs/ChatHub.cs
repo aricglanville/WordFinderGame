319bb3c3df454c3a1bf77db6aee66bb06cf43bc8
Kolby Croshaw <kolbycroshaw@gmail.com>
2022-08-02 00:48:31 -0600

    Got scoring working along with a page that displays the score and the words that were input by each user.

--- FinalProject/Hubs/ChatHub.cs
+++ FinalProject/Hubs/ChatHub.cs
@@ -14,6 +14,18 @@ namespace FinalProject.Hubs
 
         public static int count = 0;
 
+        public static List<string> connectionIDS = new List<string>();
+
+        public static List<string> user1Words = new List<String>();
+
+        public static List<string> user2Words = new List<String>();
+
+        public static int user1Score = 0;
+
+        public static int user2Score = 0;
+
+        //public static Dictionary<string, int> userScores = new Dictionary<string, int>();
+
         public async Task JoinGame(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
@@ -22,36 +34,88 @@ namespace FinalProject.Hubs
         public override async Task OnConnectedAsync()
         {
             count++;
-            var userName = Context.User.Identity.Name;
-            await base.OnConnectedAsync();
 
-            if (count >= 2)
+            await base.OnConnectedAsync();
+            connectionIDS.Add(Context.ConnectionId);
+            //userScores.Add(Context.ConnectionId, 0);
+
+            if(count < 2)
+            {
+                await Clients.All.SendAsync("HideGrid");
+            }
+            else if (count == 2)
             {   
                 await Clients.All.SendAsync("ShowGrid");                
             }
             else
             {
-                await Clients.All.SendAsync("HideGrid");
+                await Clients.Client(Context.ConnectionId).SendAsync("GameFull");
             }
         }
 
         public async Task CheckWord(string word)
         {
             var currentConnection = Context.ConnectionId;
+            //var currentScore = userScores[Context.ConnectionId];
 
             apiClass = new ApiClass(word);
             realWord = apiClass.check;
 
             if (realWord == true)
             {
-                //TODO: add scoring stuff here?
-
+                if (word.Length == 3)
+                {
+                    AddScoreAndWord(currentConnection, word, 1);
+                }
+                else if (word.Length == 4)
+                {
+                    AddScoreAndWord(currentConnection, word, 2);
+                }
+                else if (word.Length == 5)
+                {
+                    AddScoreAndWord(currentConnection, word, 4);
+                }
+                else if (word.Length == 6)
+                {
+                    AddScoreAndWord(currentConnection, word, 6);
+                }
+                else if (word.Length == 7)
+                {
+                    AddScoreAndWord(currentConnection, word, 8);
+                }
+                else if (word.Length == 8)
+                {
+                    AddScoreAndWord(currentConnection, word, 10);
+                }
+                else if (word.Length >= 9)
+                {
+                    AddScoreAndWord(currentConnection, word, 15);
+                }
                 await Clients.Client(currentConnection).SendAsync("Correct");
             }
             else
             {
                 //notify user
                 await Clients.Client(currentConnection).SendAsync("NotAWord");
+            }
+        }
+
+        public async Task GameOver()
+        {
+            await Clients.All.SendAsync("DisplayScores", user1Score, user2Score, user1Words, user2Words);
+        }
+
+        private void AddScoreAndWord(string connectionID, string word, int score)
+        {
+            if (Context.ConnectionId == connectionIDS[0])
+            {
+                user1Words.Add(word);
+                user1Score += score;
+            }
+            else if(Context.ConnectionId == connectionIDS[1])
+            {
+                user2Words.Add(word);
+                user2Score += score;
             }
         }
     }
